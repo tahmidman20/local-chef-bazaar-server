0f1e2d5c5354f47e2bbb00bcc70d1724fc76251a
tahmidman20 <tahmid7282@gmail.com>
2026-05-16 23:56:23 +0800

    fix mongodb connection for production

--- index.js
+++ index.js
@@ -72,7 +72,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+    console.log("Connected to MongoDB");
 
     const db = client.db("mealsDB");
     const mealsCollection = db.collection("meals");
@@ -620,9 +621,8 @@ async function run() {
     // console.log(
     //   "Pinged your deployment. You successfully connected to MongoDB!"
     // );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    console.error("Database connection error:", error);
   }
 }
 run().catch(console.dir);
